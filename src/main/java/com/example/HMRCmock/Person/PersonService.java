cea303b280bfdd69b6cb8ea0485af2d97e246fbf
William Burdett <williamburdett1995@gmail.com>
2022-01-30 15:41:07 +0000

    Started with SpringBoot complete!

--- src/main/java/com/example/HMRCmock/Person/PersonService.java
+++ src/main/java/com/example/HMRCmock/Person/PersonService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -19,14 +21,14 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public List<Person> getPersons(){
+    public List<Person> getPersons() {
         return personRepository.findAll();
     }
 
     public void addNewPerson(Person person) {
         Optional<Person> personOptional = personRepository.
                 findPersonByEmail(person.getEmail());
-        if (personOptional.isPresent()){
+        if (personOptional.isPresent()) {
             throw new IllegalStateException("name taken");
         }
         personRepository.save(person);
@@ -34,7 +36,7 @@ public class PersonService {
 
     public void deletePerson(Long personId) {
         boolean exists = personRepository.existsById(personId);
-        if (!exists){
+        if (!exists) {
             throw new IllegalStateException(
                     "Person with id " + personId + " does not exist."
             );
@@ -42,7 +44,21 @@ public class PersonService {
         personRepository.deleteById(personId);
     }
 
-    public void updateStudent(Long personId, String personName, String personEmail){
-
+    @Transactional
+    public void updateStudent(Long personId, String firstName, String secondName, String email) {
+        Person person = personRepository.findById(personId)
+                .orElseThrow(() -> new IllegalStateException("Person with id " + personId + " does not exist."));
+        if (firstName != null && secondName != null && firstName.length() > 0 && secondName.length() > 0 &&
+            !Objects.equals(person.getFirstName(), firstName) && !Objects.equals(person.getLastName(), secondName)){
+            person.setFirstName(firstName);
+            person.setLastName(secondName);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(person.getEmail(), email)){
+            Optional<Person> personOptional = personRepository.findPersonByEmail(email);
+                    if (personOptional.isPresent()){
+                        throw new IllegalStateException();
+            }
+                    person.setEmail(email);
+        }
     }
 }
